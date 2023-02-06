36f3f91e1fed99a5879f1323fe767247630391fd
NgocQN <104126045+NQN27@users.noreply.github.com>
2023-02-06 22:34:12 +0700

    Update schedule_cp.py

--- schedule_cp.py
+++ schedule_cp.py
@@ -1,164 +1,178 @@
-from queue import PriorityQueue
-import sys
+from ortools.sat.python import cp_model
 # Create input
+import time
 teacher= {}
 class_sub = {}
 period_sub = {}
-[T, N, M] = [int(x) for x in sys.stdin.readline().split()]
-for i in range(1, N+1):
-        class_sub[i] ={int(x) for x in sys.stdin.readline().split()[:-1]}
-for j in range(N+1, T+N+1):
-        teacher[j-N] = {int(x) for x in sys.stdin.readline().split()[:-1]}
-period_sub = [int(x) for x in sys.stdin.readline().split()]
 
-
+with open('data.txt', "r") as f:
+    lines = f.readlines()
+    T, N, M = [int(x) for x in lines[0].split()]
+    for i in range(1, N+1):
+        class_sub[i] =set(map(int,lines[i].split()[:-1]))
+    for j in range(N+1, T+N+1):
+        teacher[j-N] = set(map(int,lines[j].split()[:-1]))
+    period_sub = list(map(int,lines[T+N+1].split()))
 period_sub.insert(0,0)
 
-#Construct list of C and priority
-C = []
-mark = [[0 for sessons in range(11)] for room in range(N+1)]
-for c in class_sub:
-    for sub in class_sub[c]:
-        p = period_sub[sub]
-        C.append((sub,c,p)) #containing subject-class tuples
-        
-C= sorted(C, key = lambda x: x[2] and x[1] ,reverse=True)
-print(C)
-teacher_queue = PriorityQueue()
+start_time = time.time()
+sub_teacher_can_teach = [set() for i in range(T+1)]
+tea_sub_cla_can_learn =[set() for i in range(N+1)]
 for t in teacher:
-    teacher_queue.put((0,t,[0 for sessons in range(11)]))
+    tea = teacher[t]
+    for c in class_sub:
+        subject = class_sub[c]
+        for sub in subject:
+            if sub in tea :
+                sub_teacher_can_teach[t].add((c,sub))
+                tea_sub_cla_can_learn[c].add((t,sub))
+
+# Modelling
+
+model = cp_model.CpModel()
+Period = 6
+Session = 10
+
+# Creates Schedule variables.
+# Schedule[(t, c, p, s)]: Teacher {t} teach class {c} subject {sub} at period {p} in session {s}.
+Schedule = {}
+for t in teacher:
+    cla_sub = sub_teacher_can_teach[t]
+    for (c,sub) in cla_sub:
+        for s in range(1, Session+1):
+            for p in range(1, Period+1):
+
+                    Schedule[(t,c,sub,p,s)] = model.NewBoolVar(f'Schedule {t} {c} {sub} {p} {s}')
+
+# Create constraints
+
+# Teacher can only teach the class have the subject they can teach
+
+# Each teacher teach atmost one class in 1 period
+for t in teacher:
+    cla_sub = sub_teacher_can_teach[t]
+    for s in range(1, Session+1):
+        for p in range(1, Period+1):
+
+            model.AddAtMostOne(Schedule[(t, c, sub, p, s)] for (c,sub) in cla_sub)
+
+# Each class can learn atmost one subject in 1 period
+for c in class_sub:
+    t_sub =  tea_sub_cla_can_learn[c]
+    for s in range(1, Session+1):
+        for p in range(1, Period+1):
+            
+            model.AddAtMostOne(Schedule[(t, c, sub, p, s)] for (t,sub) in t_sub )
 
 
-def select(name_teacher,classes_teacher):
-    global C
-    for y in C:
-
-        if (y[0] in teacher[name_teacher]):
-            for i in range(1,11):
-                 if (mark[y[1]][i]+y[2]<=6) and (classes_teacher[i]+y[2]<=6) and mark[y[1]][i] == classes_teacher[i]:
-
-                     mark[y[1]][i] = mark[y[1]][i]+y[2]
-                     classes_teacher[i] = classes_teacher[i]+y[2]
-
-                     return y,(mark[y[1]][i]-y[2]+1,i),classes_teacher
-    #print('cant select s-c for teacher',t,candidates)
-
-    return None,None,None
-def select2(name_teacher,classes_teacher):
-    global C
-    for y in C:
-
-        if (y[0] in teacher[name_teacher]):
-            for i in range(1,11):
-                 if (mark[y[1]][i]+y[2]<=6) and (classes_teacher[i]+y[2]<=6) :
-                    if mark[y[1]][i] >= classes_teacher[i]:
-                        classes_teacher[i] = mark[y[1]][i]+y[2]
-                        mark[y[1]][i] = mark[y[1]][i]+y[2]
-                    else:
-                        mark[y[1]][i] = classes_teacher[i]+y[2]
-                        classes_teacher[i] = classes_teacher[i]+y[2]
-                    return y,(mark[y[1]][i]-y[2]+1,i),classes_teacher  
-                
-    return None,None,None  
-def Greedy():
-    global C
-    S = []
-    # start classes for teacher in each session
-    # start classes for room in each session
-    
-    while len(C)>0:
-         # mark classes that have already scheduled (in current iteration)
-        temp = []
-        check=[]
-        while not teacher_queue.empty():
-        
-            t = teacher_queue.get()
-            check.append(t)
-        #print('run here',t[0],t[1])
-            x,s,c_t = select(t[1],t[2])
-            if x==None:
-                temp.append(t)
-
-                continue 
-                #continue
-            S.append((x[1],x[0],s[0]+6*(s[1]-1),t[1]))
-            C.remove(x)
-                #print(t[0],t[1],x[3],c_t)
-                #temp.append((t[0]+x[3],t[1],c_t))
-            temp.append((t[0]+x[2],t[1],c_t))
-        if check==temp:
-            for t in temp:
-                teacher_queue.put(t) 
-            return S
-        for t in temp:
-            teacher_queue.put(t) 
-     
-    return S
-
-def improvedGreedy():
-    global C
-    S = []
-# start classes for teacher in each session
-    # start classes for room in each session
-    
-    while len(C)>0:
-         # mark classes that have already scheduled (in current iteration)
-        temp = []
-        check=[]
-        while not teacher_queue.empty():
-        
-            t = teacher_queue.get()
-            check.append(t)
-        #print('run here',t[0],t[1])
-            x,s,c_t = select2(t[1],t[2])
-
-            if x==None:
-                temp.append(t)
-
-                continue 
-                #continue
-            S.append((x[1],x[0],s[0]+6*(s[1]-1),t[1]))
-
-            C.remove(x)
-                #print(t[0],t[1],x[3],c_t)
-                #temp.append((t[0]+x[3],t[1],c_t))
-            temp.append((t[0]+x[2],t[1],c_t))
-        if check==temp:
-            return S
-        for t in temp:
-            teacher_queue.put(t) 
-     
-    return S
-
-S1 = Greedy()
-S2 = improvedGreedy()
-S=[]
-
-S=S1+S2
-S.sort()
-print('----------------------------------------------------------------')
-print(len(S))
-with open('data.txt','w') as f:
-    
-    f.write(str(len(S))+'\n')
-    for i in S:
-        for v in i:
-            f.write(str(v)+" ")
-        f.write('\n')
-    f.close()
+# Each class-sub can learn max once time a week 
+for c in class_sub:
+    subject = class_sub[c]
+    for sub in subject:                
+        model.Add(sum(Schedule[(t, c, sub, p, s)] 
+        for s in range(1, Session+1) for p in range(1, Period+1)
+        for t in teacher if sub in teacher[t]) <= period_sub[sub])
 '''
-37
-1 3 1 1
-1 4 7 2
-1 9 13 4
-1 10 5 4
-2 1 5 1
-2 5 7 1
-2 10 1 4
-3 8 2 3
-3 9 7 4
-4 3 13 1
-4 4 2 2
-4 6 7...
+# Each teacher teach exactly 1 class-sub, Subjects must to be teached done in 1 Session
+for t in teacher:
+    cla_sub = sub_teacher_can_teach[t]
+    
+    for (c,sub) in cla_sub:
+        
+        for s in range(1,Session+1):
+                teach = model.NewBoolVar('t')
+                sub_pe = sum(Schedule[(t, c, sub, p, s)] 
+                for p in range(1, Period+1))
+                
+                model.Add(sub_pe == period_sub[sub] ).OnlyEnforceIf(teach)
+                model.Add(sub_pe == 0).OnlyEnforceIf(teach.Not())
+'''              
 
-    '''
+
+# The periods of the class-subject must be adjacent
+for s in range(1, Session + 1):   
+    for t in teacher:
+        cla_sub = sub_teacher_can_teach[t]
+        for (c,sub) in cla_sub:
+                    pe = period_sub[sub]
+
+                    model.Add(sum(Schedule[(t, c, sub, p_, s)]
+                            for p_ in range(1,1+pe)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, 1, s)])
+
+                    model.Add(sum(Schedule[(t, c, sub, p_, s)]
+                            for p_ in range(Period+1-pe,Period+1)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, Period, s)]) 
+
+                    for p in range(2,Period+2-pe):  
+                            model.Add(sum(Schedule[(t, c, sub, p_, s)] 
+                            for p_ in range(p,p+pe)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, p, s)],Schedule[(t, c, sub, p-1, s)].Not())
+                            
+                    for p in range(Period+2-pe,Period):
+                        model.Add(sum(Schedule[(t, c, sub, p_, s)] 
+                            for p_ in range(p+1-pe,p+1)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, p, s)],Schedule[(t, c, sub, p+1, s)].Not())
+
+
+
+
+# Objective function
+model.Maximize(sum(Schedule[(t, c, sub, p, s)] for t in teacher for (c,sub) in sub_teacher_can_teach[t] for p in range(1,Period+1) for s in range(1,Session+1)))
+
+# Solver
+solver = cp_model.CpSolver()
+
+status = solver.Solve(model)
+end_time = time.time()
+count = 0
+sol =[]
+
+def Solution(c,s,sub,t):
+    global count
+    for p in range(1, Period + 1):
+        
+        if solver.Value(Schedule[(t, c, sub, p, s)]):
+                                    
+                                    count += 1
+                                    return [c,sub,p+(s-1)*6,t]
+    return None
+
+if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+    for t in teacher:
+        cla_sub = sub_teacher_can_teach[t]
+        for (c,sub) in cla_sub:
+            for s in range(1,Session+1):
+                        x = Solution(c,s,sub,t)
+                        if x != None:
+                            sol.append(x)
+                
+else:
+    print('No solution found.')
+
+def PrintSolution():
+    print(count)
+    for x in sol:
+        for i in x:
+            print(i,end=' ')
+        print()
+
+PrintSolution()
+elapsed_time = end_time - start_time
+print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+'''
+if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+    for s in range(1, Session+1):
+                print('Session ' + str(s))
+                print('-'*70)
+                for t in teacher:
+                    cla_sub = sub_teacher_can_teach[t]
+                    for (c,sub) in cla_sub:
+                    
+                    
+    
+                            for p in range(1, Period + 1):
+                                if solver.Value(Schedule[(t, c, sub, p, s)]):
+                                    print('Teacher ' + str(t) + ' teach class ' +
+                                          str(c) + ' the subject ' + str(sub) + ' at period ' + str(p))
+                print('-'*70)
+    print()
+else:
+    print('No solution found.')
+'''

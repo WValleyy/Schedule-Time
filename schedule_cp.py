eba1b1e25dbd1617744a3e2de08ec7aa84daa73a
WValleyy <101030063+WValleyy@users.noreply.github.com>
2023-02-07 22:38:02 +0700

    Update schedule_cp.py

--- schedule_cp.py
+++ schedule_cp.py
@@ -1,6 +1,6 @@
 from ortools.sat.python import cp_model
+
 # Create input
-import time
 teacher= {}
 class_sub = {}
 period_sub = {}
@@ -15,17 +15,16 @@ with open('data.txt', "r") as f:
     period_sub = list(map(int,lines[T+N+1].split()))
 period_sub.insert(0,0)
 
-start_time = time.time()
-sub_teacher_can_teach = [set() for i in range(T+1)]
-tea_sub_cla_can_learn =[set() for i in range(N+1)]
+cla_sub_teacher = [set() for i in range(T+1)]
+tea_sub = [set() for i in range(N+1)]
 for t in teacher:
     tea = teacher[t]
-    for c in class_sub:
-        subject = class_sub[c]
-        for sub in subject:
-            if sub in tea :
-                sub_teacher_can_teach[t].add((c,sub))
-                tea_sub_cla_can_learn[c].add((t,sub))
+    for x in class_sub:
+        subject = class_sub[x]
+        for y in subject:
+            if y in tea :
+                cla_sub_teacher[t].add((x,y))
+                tea_sub[x].add((t,y))
 
 # Modelling
 
@@ -34,114 +33,115 @@ Period = 6
 Session = 10
 
 # Creates Schedule variables.
-# Schedule[(t, c, p, s)]: Teacher {t} teach class {c} subject {sub} at period {p} in session {s}.
+# Schedule[(t, c, p, s)]: Teacher {t} teach class {x} subject {y} at period {u} in session {s}.
 Schedule = {}
 for t in teacher:
-    cla_sub = sub_teacher_can_teach[t]
-    for (c,sub) in cla_sub:
+    cla_sub = cla_sub_teacher[t]
+    for (x,y) in cla_sub:
         for s in range(1, Session+1):
-            for p in range(1, Period+1):
+            for u in range(1, Period+1):
 
-                    Schedule[(t,c,sub,p,s)] = model.NewBoolVar(f'Schedule {t} {c} {sub} {p} {s}')
+                    Schedule[(t, x, y, u, s)] = model.NewBoolVar(f'Schedule {t} {x} {y} {u} {s}')
 
 # Create constraints
 
-# Teacher can only teach the class have the subject they can teach
-
-# Each teacher teach atmost one class in 1 period
+# Each teacher teach atmost 1 class in 1 period
 for t in teacher:
-    cla_sub = sub_teacher_can_teach[t]
+    cla_sub = cla_sub_teacher[t]
     for s in range(1, Session+1):
-        for p in range(1, Period+1):
+        for u in range(1, Period+1):
 
-            model.AddAtMostOne(Schedule[(t, c, sub, p, s)] for (c,sub) in cla_sub)
+            model.AddAtMostOne(Schedule[(t, x, y, u, s)] for (x,y) in cla_sub)
 
-# Each class can learn atmost one subject in 1 period
-for c in class_sub:
-    t_sub =  tea_sub_cla_can_learn[c]
+# Each class can learn atmost 1 subject in 1 period
+for x in class_sub:
+    t_sub =  tea_sub[x]
     for s in range(1, Session+1):
-        for p in range(1, Period+1):
+        for u in range(1, Period+1):
             
-            model.AddAtMostOne(Schedule[(t, c, sub, p, s)] for (t,sub) in t_sub )
+            model.AddAtMostOne(Schedule[(t, x, y, u, s)] for (t,y) in t_sub)
 
 
 # Each class-sub can learn max once time a week 
-for c in class_sub:
-    subject = class_sub[c]
-    for sub in subject:                
-        model.Add(sum(Schedule[(t, c, sub, p, s)] 
-        for s in range(1, Session+1) for p in range(1, Period+1)
-        for t in teacher if sub in teacher[t]) <= period_sub[sub])
-'''
-# Each teacher teach exactly 1 class-sub, Subjects must to be teached done in 1 Session
-for t in teacher:
-    cla_sub = sub_teacher_can_teach[t]
-    
-    for (c,sub) in cla_sub:
-        
-        for s in range(1,Session+1):
-                teach = model.NewBoolVar('t')
-                sub_pe = sum(Schedule[(t, c, sub, p, s)] 
-                for p in range(1, Period+1))
-                
-                model.Add(sub_pe == period_sub[sub] ).OnlyEnforceIf(teach)
-                model.Add(sub_pe == 0).OnlyEnforceIf(teach.Not())
-'''              
+for x in class_sub:
+    subject = class_sub[x]
+    for y in subject:                
+        model.Add(sum(Schedule[(t, x, y, u, s)] 
+        for s in range(1, Session+1) for u in range(1, Period+1)
+        for t in teacher if y in teacher[t]) <= period_sub[y])
+            
 
-
-# The periods of the class-subject must be adjacent
-for s in range(1, Session + 1):   
+# The periods of the class-subject must be adjacent and each class-subject learn full of periods
+for s in range(1, Session+1):   
     for t in teacher:
-        cla_sub = sub_teacher_can_teach[t]
-        for (c,sub) in cla_sub:
-                    pe = period_sub[sub]
+        cla_sub = cla_sub_teacher[t]
+        for (x,y) in cla_sub:
+                    pe = period_sub[y]
+                    if pe == 6:
+                        teach = model.NewBoolVar('t')
+                        model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(1,Period+1)) == pe).OnlyEnforceIf(teach)
+                        model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(1, 1+pe)) == 0).OnlyEnforceIf(teach.Not())
 
-                    model.Add(sum(Schedule[(t, c, sub, p_, s)]
-                            for p_ in range(1,1+pe)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, 1, s)])
+                    elif pe == 5:
+                        teach =model.NewBoolVar('t')
+                        tea = model.NewBoolVar('t')
 
-                    model.Add(sum(Schedule[(t, c, sub, p_, s)]
-                            for p_ in range(Period+1-pe,Period+1)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, Period, s)]) 
+                        model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(1, Period)) == pe).OnlyEnforceIf(tea).OnlyEnforceIf(teach)
 
-                    for p in range(2,Period+2-pe):  
-                            model.Add(sum(Schedule[(t, c, sub, p_, s)] 
-                            for p_ in range(p,p+pe)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, p, s)],Schedule[(t, c, sub, p-1, s)].Not())
+                        model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(2, Period+1)) == pe).OnlyEnforceIf(tea.Not()).OnlyEnforceIf(teach) 
+                        
+                        model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(1, Period+1)) == 0).OnlyEnforceIf(teach.Not())
+                    else: 
+                        model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(1, 1+pe)) == pe).OnlyEnforceIf(Schedule[(t, x, y, 1, s)])
+
+                        model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(Period+1-pe, Period+1)) == pe).OnlyEnforceIf(Schedule[(t, x, y, Period, s)]) 
+
+                        for u in range(2,Period+2-pe):  
+                            model.Add(sum(Schedule[(t, x, y, u_, s)] 
+                            for u_ in range(u,u+pe)) == pe).OnlyEnforceIf(Schedule[(t, x, y, u, s)]).OnlyEnforceIf(Schedule[(t, x, y, u-1, s)].Not())
                             
-                    for p in range(Period+2-pe,Period):
-                        model.Add(sum(Schedule[(t, c, sub, p_, s)] 
-                            for p_ in range(p+1-pe,p+1)) == pe).OnlyEnforceIf(Schedule[(t, c, sub, p, s)],Schedule[(t, c, sub, p+1, s)].Not())
+                        for u in range(Period+2-pe,Period):
+                            model.Add(sum(Schedule[(t, x, y, u_, s)]
+                            for u_ in range(u+1-pe, u+1)) == pe).OnlyEnforceIf(Schedule[(t, x, y, u, s)]).OnlyEnforceIf(Schedule[(t, x, y, u+1, s)].Not())
 
 
 
 
 # Objective function
-model.Maximize(sum(Schedule[(t, c, sub, p, s)] for t in teacher for (c,sub) in sub_teacher_can_teach[t] for p in range(1,Period+1) for s in range(1,Session+1)))
+model.Maximize(sum(Schedule[(t, x, y, u, s)] for t in teacher for (x,y) in cla_sub_teacher[t] for u in range(1,Period+1) for s in range(1,Session+1)))
 
 # Solver
 solver = cp_model.CpSolver()
 
 status = solver.Solve(model)
-end_time = time.time()
 count = 0
 sol =[]
 
-def Solution(c,s,sub,t):
+def Solution(t, x, y, s):
     global count
-    for p in range(1, Period + 1):
+    for u in range(1, Period + 1):
         
-        if solver.Value(Schedule[(t, c, sub, p, s)]):
+        if solver.Value(Schedule[(t, x, y, u, s)]):
                                     
-                                    count += 1
-                                    return [c,sub,p+(s-1)*6,t]
+            count += 1
+            return [x,y,u+(s-1)*6,t]
     return None
 
 if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
     for t in teacher:
-        cla_sub = sub_teacher_can_teach[t]
-        for (c,sub) in cla_sub:
+        cla_sub = cla_sub_teacher[t]
+        for (x,y) in cla_sub:
             for s in range(1,Session+1):
-                        x = Solution(c,s,sub,t)
-                        if x != None:
-                            sol.append(x)
+                    z = Solution(t, x, y, s)
+                    if z != None:
+                        sol.append(z)
                 
 else:
     print('No solution found.')
@@ -152,27 +152,4 @@ def PrintSolution():
         for i in x:
             print(i,end=' ')
         print()
-
 PrintSolution()
-elapsed_time = end_time - start_time
-print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
-'''
-if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-    for s in range(1, Session+1):
-                print('Session ' + str(s))
-                print('-'*70)
-                for t in teacher:
-                    cla_sub = sub_teacher_can_teach[t]
-                    for (c,sub) in cla_sub:
-                    
-                    
-    
-                            for p in range(1, Period + 1):
-                                if solver.Value(Schedule[(t, c, sub, p, s)]):
-                                    print('Teacher ' + str(t) + ' teach class ' +
-                                          str(c) + ' the subject ' + str(sub) + ' at period ' + str(p))
-                print('-'*70)
-    print()
-else:
-    print('No solution found.')
-'''
